bb0be7701181ff7fe8ab35d7d2623a0ca868fc38
Umar Hayyat <umarhayyat71.com>
2025-03-09 15:49:48 +0500

    some code is added

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-import menuArray from "./data"
+import menuArray from "./data.js"
+
 
 const menu = document.getElementById('menu')
 
-const items = menuArray.map((item) =>{
-    const {name, ingredients, id, price, emoji} = menuArray
-    
-    return `
-   <div class="menu-items">
-                <img src="${emoji}" alt="" class="item-graphic">
-                <div class="item-content">
-                    <h2 class="item-title">${name}</h2>
-                    <p class="item-description">INGREDIENTS</p>
-                    <h3 class="item-price">${price}</h3>
-                </div>
-                <button class="add-btn"><i class="fa-regular fa-plus"></i></button>
-            </div>
-    `
-})
+function feedHtml() {
 
-menu.innerHTML += items
+    const items = menuArray.map((item) =>{
+     
+        const {name, price, emoji,ingredients} = item
+        
+        
+        return `
+       <div class="menu-items">
+                    <div class="item-graphic">${emoji}</div>
+                    <div class="item-content">
+                        <h2 class="item-title">${name}</h2>
+                        <p class="item-description">${[...ingredients]}</p>
+                        <h3 class="item-price">$${price}</h3>
+                    </div>
+                    <button class="add-btn"><i class="fa-regular fa-plus"></i></button>
+                </div>
+        `
+    }).join('')
+    return items
+}
+
+
+
+function render(){
+    menu.innerHTML = feedHtml()
+}
+
+render()
